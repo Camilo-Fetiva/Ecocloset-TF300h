78069ca528217164a6ff700241cb5192fe5fc7dc
Camilo-Fetiva <kmilofetiva@gmail.com>
2024-11-29 21:29:09 -0500

    [actualizar] componentes pag, rutas, navegacion

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+
+// DEPENDENCIAS NECESARIAS
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink], //<- USO EN EL COMPONENT
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
