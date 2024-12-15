288fd88d989d13fce789c7b67f6147c4ebfdb27e
Camilo-Fetiva <kmilofetiva@gmail.com>
2024-12-15 18:18:06 -0500

    [actualizar] producto especifico

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 import { ProductSpecificComponent } from '../../components/product-specific/product-specific.component';
 
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [ProductSpecificComponent],
+  imports: [ProductSpecificComponent, ProductCardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
